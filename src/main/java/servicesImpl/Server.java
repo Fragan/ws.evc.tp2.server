603bd3e79a866c144b0c8498592c854e868c1c27
Taira <taira@taira-R500VJ.(none)>
2013-01-22 17:12:58 +0100

    updates

--- src/main/java/servicesImpl/Server.java
+++ src/main/java/servicesImpl/Server.java
@@ -1,14 +1,20 @@
 package servicesImpl;
 
 import j3d.abstraction.universe.ASharedUniverse;
+import j3d.interfaces.universe.ICamera;
+import j3d.interfaces.universe.IObject;
 import j3d.interfaces.universe.ISharedUniverse;
+import j3d.interfaces.universe.ISharedUniverseServer;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
-public class Server extends UnicastRemoteObject {
+import broadcast.BroadcastUpdates;
+
+public class Server extends UnicastRemoteObject implements ISharedUniverseServer {
 
 	private static final long serialVersionUID = -2752486228990221680L;
 
@@ -26,7 +32,7 @@ public class Server extends UnicastRemoteObject {
 			// "createRegistry"
 			LocateRegistry.createRegistry(serverRMIPort);
 			Naming.rebind("//" + serverHostName + ":" + serverRMIPort + "/"
-					+ sharedWorldName, sharedUniverse);
+					+ sharedWorldName, this);
 			System.out.println("Ready...");
 			
 		} catch (Exception e) {
@@ -34,5 +40,66 @@ public class Server extends UnicastRemoteObject {
 			System.err.println("Problem youston");
 		}
 	}
+
+	public ArrayList<IObject> getObjects() throws RemoteException {
+		ArrayList<IObject> l = new ArrayList<IObject>();
+		l.addAll(sharedUniverse.getObjects());
+		return l;
+	}
+
+	public ArrayList<ICamera> getCameras() throws RemoteException {
+		ArrayList<ICamera> l = new ArrayList<ICamera>();
+		l.addAll(sharedUniverse.getCameras());
+		return l;
+	}
+
+	public boolean add(IObject object) throws RemoteException {
+		return sharedUniverse.add(object);
+	}
+
+	public void remove(IObject object) throws RemoteException {
+		sharedUniverse.remove(object);
+	}
+
+	public boolean add(ICamera camera) throws RemoteException {
+		return sharedUniverse.add(camera);
+	}
+
+	public void remove(ICamera camera) throws RemoteException {
+		sharedUniverse.remove(camera);
+	}
+
+	public IObject getObject(String name) throws RemoteException {
+		return sharedUniverse.getObject(name);
+	}
+
+	public ICamera getCamera(String name) throws RemoteException {
+		return sharedUniverse.getCamera(name);
+	}
+
+	public String getName() throws RemoteException {
+		return sharedUniverse.getName();
+	}
+
+	public void update(ICamera camera) throws RemoteException {
+		ICamera ourCamera = sharedUniverse.getCamera(camera.getOwnerName());
+		if (ourCamera != null) {
+			ourCamera.setOrientation(camera.getOrientation());
+			ourCamera.setPosition(camera.getPosition());
+			BroadcastUpdates.getInstance().diffuse(camera);
+		}
+	}
+
+	public void update(IObject object) throws RemoteException {
+		IObject ourObject = sharedUniverse.getObject(object.getName());
+		if (ourObject != null) {
+			ourObject.setOrientation(object.getOrientation());
+			ourObject.setPosition(object.getPosition());
+			System.out.println(BroadcastUpdates.instance);
+			BroadcastUpdates.getInstance().diffuse(object);
+		}
+	}
+	
+	
 
 }
